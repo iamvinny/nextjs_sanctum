e186cf8c9d80a021f3fd96afb29e465531addd2c
Vinny <vinny@cubetechnology.org>
2024-10-17 02:42:02 -0400

    Update GuestLayout.tsx

--- layouts/GuestLayout.tsx
+++ layouts/GuestLayout.tsx
@@ -1,18 +1,19 @@
 "use client";
 
+import { usePathname, useRouter } from "next/navigation";
 import Footer from "@/components/client/Footer";
 import Navbar from "@/components/client/Navbar";
 import { useAuth } from "@/context/AuthContext";
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function GuestLayout({ children }: { children: React.ReactNode }) {
     const router = useRouter();
+    const pathname = usePathname();
     const { user, loading } = useAuth();
     
     // Redirect to feed if user is logged in
     useEffect(() => {
-        if (!loading && user) {
+        if (!loading && user && pathname === '/login' || pathname === '/register') {
             router.push('/feed');
         }
     }, [loading, user, router]);
